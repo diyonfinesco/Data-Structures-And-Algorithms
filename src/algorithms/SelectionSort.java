115c7d8d8b99584b3fa40e1c14200f622a1ff33c
diyonfinesco <chanaka.s@icloud.com>
2024-01-23 11:15:02 +0100

    add SelectionSort

---
+++ src/algorithms/SelectionSort.java
@@ -0,0 +1,23 @@
+package algorithms;
+
+public class SelectionSort {
+    public static void sort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int index = i;
+
+            for (int j = i; j < array.length; j++) {
+                if(array[j] < array[index]){
+                    index = j;
+                }
+            }
+
+            swap(array, i, index);
+        }
+    }
+
+    private static void swap(int[] array, int firstIndex, int secondIndex) {
+        int temp = array[firstIndex];
+        array[firstIndex] = array[secondIndex];
+        array[secondIndex] = temp;
+    }
+}
