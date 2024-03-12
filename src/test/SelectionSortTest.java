344fb5d4eac994146e45f725108dd34a93f6921c
Chanaka Subasinghage <chanaka.s@icloud.com>
2024-03-12 19:17:34 +0100

    add unit test cases for SelectionSortTest

---
+++ src/test/SelectionSortTest.java
@@ -0,0 +1,83 @@
+package test;
+
+import algorithms.SelectionSort;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SelectionSortTest {
+
+    @Test
+    void shouldSortSmallArray() {
+        int[] array = {5, 2, 4, 6, 1, 3};
+        SelectionSort.sort(array);
+        Assertions.assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6}, array);
+    }
+
+    @Test
+    void shouldSortLargeArray() {
+        int[] array = new int[1000];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = array.length - i;
+        }
+        SelectionSort.sort(array);
+        for (int i = 1; i <= array.length; i++) {
+            Assertions.assertEquals(i, array[i - 1]);
+        }
+    }
+
+    @Test
+    void shouldSortArrayWithNegatives() {
+        int[] array = {-3, -1, -4, -2, 0};
+        SelectionSort.sort(array);
+        Assertions.assertArrayEquals(new int[]{-4, -3, -2, -1, 0}, array);
+    }
+
+    @Test
+    void shouldSortArrayWithDuplicates() {
+        int[] array = {5, 2, 2, 3, 3, 1};
+        SelectionSort.sort(array);
+        Assertions.assertArrayEquals(new int[]{1, 2, 2, 3, 3, 5}, array);
+    }
+
+    @Test
+    void shouldSortAlreadySortedArray() {
+        int[] array = {1, 2, 3, 4, 5};
+        SelectionSort.sort(array);
+        Assertions.assertArrayEquals(new int[]{1, 2, 3, 4, 5}, array);
+    }
+
+    @Test
+    void shouldSortReverseSortedArray() {
+        int[] array = {5, 4, 3, 2, 1};
+        SelectionSort.sort(array);
+        Assertions.assertArrayEquals(new int[]{1, 2, 3, 4, 5}, array);
+    }
+
+    @Test
+    void shouldSortArrayWithAllIdenticalElements() {
+        int[] array = {1, 1, 1, 1, 1};
+        SelectionSort.sort(array);
+        Assertions.assertArrayEquals(new int[]{1, 1, 1, 1, 1}, array);
+    }
+
+    @Test
+    void shouldSortSingleElementArray() {
+        int[] array = {1};
+        SelectionSort.sort(array);
+        Assertions.assertArrayEquals(new int[]{1}, array);
+    }
+
+    @Test
+    void shouldSortEmptyArray() {
+        int[] array = {};
+        SelectionSort.sort(array);
+        Assertions.assertArrayEquals(new int[]{}, array);
+    }
+
+    @Test
+    void shouldSortArrayWithZerosAndNegatives() {
+        int[] array = {0, -1, -3, 0, 2};
+        SelectionSort.sort(array);
+        Assertions.assertArrayEquals(new int[]{-3, -1, 0, 0, 2}, array);
+    }
+}
